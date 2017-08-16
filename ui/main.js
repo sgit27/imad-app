22ac8f512e88646f32adc08893a4759d8e01c40d
sgit27 <17807725+sgit27@users.noreply.github.com>
2017-08-16 22:53:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,21 @@
 
 var button=document.getElementById('counter');
 //alert("I am here");
-var buttonCounter = 0;
 button.onclick = function() {
-    buttonCounter = buttonCounter + 1;
-    //alert("I am here clicking button " + buttonCounter.toString());
-    var span = document.getElementById('count');
-    span.innerHTML = buttonCounter.toString();
+    
+
+//Create a request object
+var request = new XMLHttpRequest();
+//get and store the counter end point data
+request.onreadystatechange = function() {
+    if(this.readystate===4 && this.status === 200){
+        var buttonCounter = reqeust.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = buttonCounter.toString();
+    }
 };
+// make the request
+request.open("GET", "http://vpsarav.imad.hasura-app.io/counter", true);
+request.send();
+};
+
