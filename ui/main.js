a6bea36e05ab2c523feb59e35e6a71dd56c241b9
sgit27 <17807725+sgit27@users.noreply.github.com>
2017-08-16 21:16:15 +0530

    Create main.js

---
+++ ui/main.js
@@ -0,0 +1,10 @@
+
+var button = document.getElementById("counter");
+alert("I am here");
+var buttonCounter = 0;
+button.onclick = function() {
+    buttonCounter = buttonCounter + 1;
+    alert("I am here clicking button " + buttonCounter.toString());
+    var span = document.getElementById("count");
+    span.innerHTML = buttonCounter.tostring();
+};
