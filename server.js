6526c2cf8315826d28631c0b94979da62297e580
sgit27 <17807725+sgit27@users.noreply.github.com>
2017-08-16 12:38:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/FirstPage', function (req, res) {
+app.get('/FirstPage.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'FirstPage.html'));
 });
 
