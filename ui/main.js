52aba355cda75c0cd0df9922ec1325ddea7561bb
sgit27 <17807725+sgit27@users.noreply.github.com>
2017-08-16 21:13:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,4 +8,4 @@ button.onclick = function() {
     alert("I am here clicking button " + buttonCounter.toString());
     var span = document.getElementById("count");
     span.innerHTML = buttonCounter.tostring();
-}
+};
