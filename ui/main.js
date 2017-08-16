26c12d34bd216760e8c9391b8a3a08e0f18d14cd
sgit27 <17807725+sgit27@users.noreply.github.com>
2017-08-16 23:08:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var request = new XMLHttpRequest();
 //get and store the counter end point data
 request.onreadystatechange = function() {
     if(this.readyState=== XMLHttpRequest.DONE && this.status === 200){
-        var Counter = reqeust.responseText;
+        var Counter = request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
         alert("I am done with the request to get the counter end point data");
